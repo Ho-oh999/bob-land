83af7fd6800c831ad93e1ce173074d9b11a290d1
boko999 <ho-oh999@hotmail.com>
2020-07-23 19:27:24 +0000

    f

--- main.py
+++ main.py
@@ -17,3 +17,41 @@ def sphere(size):
      bob3.color(0,0,shade)
 
 sphere(100)
+
+bob4 = turtle.Turtle()
+bob4.shape("turtle")
+
+bob4.speed(0)
+bob4.pensize(3)
+
+def sphere(size):
+   shade = 75.0
+   bob4.color(0,shade,0)
+   while size > 0:
+     bob4.begin_fill
+     bob4.circle(size)
+     bob4.end_fill()
+     size -= .5
+     shade += 1
+     bob4.color(0,shade,0)
+
+sphere(100)
+
+bob5 = turtle.Turtle()
+bob5.shape("turtle")
+
+bob5.speed(0)
+bob5.pensize(3)
+
+def sphere(size):
+   shade = 75.0
+   bob5.color(0,shade,0)
+   while size > 0:
+     bob5.begin_fill
+     bob5.circle(size)
+     bob5.end_fill()
+     size -= .5
+     shade += 1
+     bob5.color(shade,0,0)
+
+sphere(100)
